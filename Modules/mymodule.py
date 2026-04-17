393f7c195f773ef237f7d16e7ac104525c8ebf0e
Mks128 <96481600+Mks128@users.noreply.github.com>
2026-04-17 17:14:41 +0000

    commit Modules/main.py

---
+++ Modules/mymodule.py
@@ -0,0 +1,13 @@
+def greet(name):
+    return f"Hello, {name}!"
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero!"
+    return a / b
+my_variable = 45
