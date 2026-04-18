8f042ea2e2cb135f79922978d3883825dc979616
Mks128 <96481600+Mks128@users.noreply.github.com>
2026-04-18 15:57:33 +0000

    commit recent changes

--- Modules/main.py
+++ Modules/main.py
@@ -1,2 +1,14 @@
 import mymodule
+name = input("Enter your name: ")
+greeting = mymodule.greet(name)
+print(greeting) 
+
+result_add = mymodule.add(10, 5)
+print(f"Addition: {result_add}")
+result_subtract = mymodule.subtract(10, 5)
+print(f"Subtraction: {result_subtract}")
+result_multiply = mymodule.multiply(10, 5)
+print(f"Multiplication: {result_multiply}")
+result_divide = mymodule.divide(10, 5)
+print(f"Division: {result_divide}")
 
