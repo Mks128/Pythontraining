6eee29a8a77ce4c4c550abb145b94b15a198f788
Mks128 <96481600+Mks128@users.noreply.github.com>
2026-05-06 16:39:44 +0000

    commit 1234567890abcdef1234567890abcdef12345678

---
+++ Interview Preparation/s1.py
@@ -0,0 +1,5 @@
+x, y = input("Enter two numbers separated by space: ").split()
+print(f"x = {x}, y = {y}")
+
+x,y = map(int, input("Enter two numbers separated by space: ").split())
+print(f"x = {x}, y = {y}, x+y = {x+y}")
